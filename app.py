cd5889231b34b7d36ab1b849a15b8ea22f7bf018
loitran1505 <loitran1505@gmail.com>
2026-03-16 23:36:54 +0700

    Demo2

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ import os
 import time
 import ast
 import json
+from scipy.spatial import KDTree
 from dijkstra_algorithm import dijkstra_search
 from dwa_star_algorithm import dwa_star_search
 from A_star_algorithm import a_star_search
 from load_graph import load_graph
 
 st.set_page_config(layout="wide", page_title="HCMC Routing Comparison")
+
+@st.cache_resource 
+def build_spatial_index(nodes_df):
+    # Lấy mảng tọa độ [[lat1, lon1], [lat2, lon2], ...]
+    points = nodes_df[['y', 'x']].values
+    return KDTree(points)
+
 #preload 
 @st.cache_data
 def get_traffic_geojson(edges_df, time_slot):
@@ -62,8 +70,8 @@ def get_traffic_geojson(edges_df, time_slot):
 
 @st.cache_data
 def load_data(time_slot):
-    n_path = r"data\nodes.csv"
-    e_path = r"data\edges.csv"
+    n_path = 'data/nodes.csv'
+    e_path = 'data/edges.csv'
     
     coords, adj ,edges_lookup = load_graph(n_path, e_path, time_slot)
 
@@ -77,8 +85,9 @@ def load_data(time_slot):
     min_lat, max_lat = nodes_df['y'].min(), nodes_df['y'].max()
     min_lon, max_lon = nodes_df['x'].min(), nodes_df['x'].max()
     bounds = [[min_lat - 0.001, min_lon - 0.001], [max_lat + 0.001, max_lon + 0.001]]
-    
-    return coords, adj, nodes_df, edges_lookup,traffic_geojson, bounds
+    spatial_index = build_spatial_index(nodes_df)
+
+    return coords, adj, nodes_df, edges_lookup,traffic_geojson, bounds , spatial_index
 
 # Chọn khung thời gian
 time_options = []
@@ -110,7 +119,7 @@ if slot != st.session_state.old_slot:
     st.rerun()
 
 # lấy dữ liệu
-coords, adj, nodes_df, edges_lookup, traffic_data,map_bounds = load_data(slot)
+coords, adj, nodes_df, edges_lookup, traffic_data,map_bounds,spatial_index = load_data(slot)
 
 if 'start_coord' not in st.session_state: st.session_state.start_coord = None
 if 'end_coord' not in st.session_state: st.session_state.end_coord = None
@@ -124,6 +133,10 @@ c1, c2 = st.sidebar.columns(2)
 with c1: 
     if st.button(" Điểm đầu",use_container_width= True): 
         st.session_state.selecting = 'start'
+        st.session_state.path_coords = None
+        st.session_state.comparison_df = None
+        st.session_state.start_coord = None
+        st.session_state.end_coord = None
         if 'main_map' in st.session_state and 'last_clicked' in st.session_state['main_map']:
              click_old = st.session_state['main_map']['last_clicked']
              if click_old:
@@ -131,6 +144,9 @@ with c1:
 with c2: 
     if st.button(" Điểm cuối",use_container_width= True): 
         st.session_state.selecting = 'end'
+        st.session_state.path_coords = None
+        st.session_state.comparison_df = None
+        st.session_state.end_coord = None
         if 'main_map' in st.session_state and 'last_clicked' in st.session_state['main_map']:
              click_old = st.session_state['main_map']['last_clicked']
              if click_old:
@@ -145,11 +161,18 @@ if st.sidebar.button("Reset", use_container_width=True):
         st.session_state.selecting = None
         st.rerun()
 
-if st.sidebar.button("Chạy tất cả", type="primary"):
+if st.sidebar.button("Chạy thuật toán", type="primary"):
     if st.session_state.start_coord and st.session_state.end_coord:
-        u_s = ((nodes_df['y']-st.session_state.start_coord[0])**2 + (nodes_df['x']-st.session_state.start_coord[1])**2).idxmin()
-        v_e = ((nodes_df['y']-st.session_state.end_coord[0])**2 + (nodes_df['x']-st.session_state.end_coord[1])**2).idxmin()
-        
+        if st.session_state.start_coord and st.session_state.end_coord:
+            u_s = st.session_state.get('start_node_id')
+            v_e = st.session_state.get('end_node_id')
+        if u_s is None or v_e is None:
+            # Dùng spatial_index (KD-Tree) để tìm lại nhanh
+            _, idx_s = spatial_index.query(st.session_state.start_coord)
+            _, idx_e = spatial_index.query(st.session_state.end_coord)
+            u_s = nodes_df.index[idx_s]
+            v_e = nodes_df.index[idx_e]
+            
         stats = []
         final_path = None
         
@@ -209,24 +232,29 @@ def show_map():
 
     out = st_folium(m, width="100%", height=600, key="main_map", returned_objects=["last_clicked"])
 
+
     if out and out.get('last_clicked'):
         click_data = out['last_clicked']
-        
         current_click_id = f"{click_data['lat']}_{click_data['lng']}"
+
         if st.session_state.selecting is not None and current_click_id != st.session_state.last_click_id:
             
-            coords = (click_data['lat'], click_data['lng'])
+           
+            dist, idx = spatial_index.query([click_data['lat'], click_data['lng']])
+            
+            # Lấy osmid và tọa độ thực của Node đó
+            nearest_node_id = nodes_df.index[idx]
+            nearest_coords = (nodes_df.iloc[idx]['y'], nodes_df.iloc[idx]['x'])
             
             if st.session_state.selecting == 'start':
-                st.session_state.start_coord = coords
+                st.session_state.start_coord = nearest_coords
+                st.session_state.start_node_id = nearest_node_id # Lưu ID để chạy thuật toán
             elif st.session_state.selecting == 'end':
-                st.session_state.end_coord = coords
+                st.session_state.end_coord = nearest_coords
+                st.session_state.end_node_id = nearest_node_id
             
             st.session_state.last_click_id = current_click_id
-
             st.session_state.selecting = None
             st.rerun()
-        elif st.session_state.selecting is None:
-            st.session_state.last_click_id = current_click_id
     
 show_map()
